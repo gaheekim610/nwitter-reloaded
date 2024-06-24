372a658c9281f1124a8c4aafc7ddb2ea770ec204
GaheeKIM <gaheekim610@gmail.com>
2024-06-24 19:39:47 +1000

    add function of login, signout and signup by using firebase

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,43 +1,14 @@
 import { useState } from "react";
-import styled from "styled-components"
+import { Link, useNavigate } from "react-router-dom";
 
-const Wrapper = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 420px;
-    padding: 50px 0px;
-`;
-const Title = styled.h1`
-    font-size: 42px;
-`;
-const Form = styled.form`
-    margin-top: 50px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    width: 100%;
-`;
-const Input = styled.input`
-    padding: 10px 20px;
-    border-radius: 50px;
-    border: none;
-    width: 100%;
-    font-size: 16px;
-    &[type="submit] {
-        cursor: pointer;
-        &:hover {
-            opacity: 0.8;
-        }
-    }
-`;
-const Error = styled.span`
-    font-weight: 600;
-    color: tomato;
-`;
+import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { FirebaseError } from "firebase/app";
+
+import { Error, Form, Input, Switcher, Title, Wrapper } from "../components/auth-component";
 
 export default function CreateAccount() {
+    const navigate = useNavigate();
     const [isLoading, setLoading] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -53,23 +24,34 @@ export default function CreateAccount() {
             setPassword(value);
         }
     };
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setError("");
+        if(isLoading||name===''||email===''||password==='') return;
         try{
+            setLoading(true);
             // create an account
+            const credentials = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(credentials.user)
             // set the name of the user
+            await updateProfile(credentials.user,{
+                displayName: name,
+            });
             // redirect to the home page
+            navigate("/")
         } catch(e){
-            // setError
+            if (e instanceof FirebaseError){
+                console.log(e.code, e.message);
+                setError(e.message)
+            }
         } finally{
             setLoading(false);
         }
-
-        console.log(name, email, password)
     }
+
     return ( 
         <Wrapper>
-            <Title> Log into X</Title>
+            <Title> Join X</Title>
             <Form onSubmit={onSubmit}>
                 <Input onChange={onChange} name="name" placeholder="Name" type="text" required />
                 <Input onChange={onChange} name="email" placeholder="Email" type="email" required/>
@@ -77,6 +59,9 @@ export default function CreateAccount() {
                 <Input type="submit" value={isLoading ? "Loading...":"Create Account"}/>
             </Form>
             {error !== "" ? <Error>{error}</Error> : null}
+            <Switcher>
+               Already have an account? <Link to="/login">Log in</Link>
+            </Switcher>
         </Wrapper>
     )
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,56 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
+import { Error, Input, Switcher, Title, Wrapper, Form } from "../components/auth-component";
+
 export default function Login() {
-    return <h1>login</h1>
+    const navigate = useNavigate();
+    const [isLoading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { target : {name, value } } = e;
+    if ( name === 'email'){
+            setEmail(value);
+        } else if (name === 'password'){
+            setPassword(value);
+        }
+    };
+    const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError("");
+        if(isLoading||email===''||password==='') return;
+        try{
+            setLoading(true);
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate("/")
+        } catch(e){
+            if (e instanceof FirebaseError){
+                console.log(e.code, e.message);
+                setError(e.message)
+            }
+        } finally{
+            setLoading(false);
+        }
+    }
+
+    return ( 
+        <Wrapper>
+            <Title> Log into X</Title>
+            <Form onSubmit={onSubmit}>
+                <Input onChange={onChange} name="email" placeholder="Email" type="email" required/>
+                <Input onChange={onChange} name="password" placeholder="Password" type="password"/>
+                <Input type="submit" value={isLoading ? "Loading...":"Log in"}/>
+            </Form>
+            {error !== "" ? <Error>{error}</Error> : null}
+            <Switcher>
+                Don't have an account? <Link to="/create-account">Create one</Link>
+            </Switcher>
+        </Wrapper>
+    )
 }
