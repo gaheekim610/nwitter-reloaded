f0f868ac118e811f0145a852562160192d402b1b
GaheeKIM <gaheekim610@gmail.com>
2024-07-02 20:05:35 +1000

    realtime data fetch

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
 
 import { db } from "../firebase";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+	collection,
+	limit,
+	// getDocs,
+	onSnapshot,
+	orderBy,
+	query,
+} from "firebase/firestore";
 
 import styled from "styled-components";
 import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
 
 export interface ITweet {
 	id: string;
@@ -15,34 +23,58 @@ export interface ITweet {
 	createdAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	gap: 10px;
+	flex-direction: column;
+`;
 
 export default function Timeline() {
 	const [tweets, setTweets] = useState<ITweet[]>([]);
-	const fetchTweets = async () => {
-		const tweetsQuery = query(
-			collection(db, "tweets"),
-			orderBy("createdAt", "desc"),
-		);
-		const snapshot = await getDocs(tweetsQuery);
 
-		console.log("snapshot", snapshot);
-		const tweetArr = snapshot.docs.map((doc) => {
-			const { tweet, createdAt, userId, username, photo } = doc.data();
-
-			return {
-				id: doc.id,
-				photo,
-				tweet,
-				userId,
-				createdAt,
-				username,
-			};
-		});
-		setTweets(tweetArr);
-	};
 	useEffect(() => {
+		let unsubscribe: Unsubscribe | null = null;
+		const fetchTweets = async () => {
+			const tweetsQuery = query(
+				collection(db, "tweets"),
+				orderBy("createdAt", "desc"),
+				limit(25),
+			);
+			// const snapshot = await getDocs(tweetsQuery);
+
+			// const tweetArr = snapshot.docs.map((doc) => {
+			// 	const { tweet, createdAt, userId, username, photo } = doc.data();
+
+			// 	return {
+			// 		id: doc.id,
+			// 		photo,
+			// 		tweet,
+			// 		userId,
+			// 		createdAt,
+			// 		username,
+			// 	};
+			// });
+			unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+				const tweetArr = snapshot.docs.map((doc) => {
+					const { tweet, createdAt, userId, username, photo } = doc.data();
+
+					return {
+						id: doc.id,
+						photo,
+						tweet,
+						userId,
+						createdAt,
+						username,
+					};
+				});
+				setTweets(tweetArr);
+			});
+		};
+
 		fetchTweets();
+		return () => {
+			unsubscribe && unsubscribe();
+		};
 	}, []);
 
 	return (
